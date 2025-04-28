a6255f575097cee80e6f51d8594d0083c6078411
Rayane Nakib (ريان نقيب) <nakibrayane@proton.me>
2025-04-28 09:19:33 +0100

    Update flake.nix to build the website with nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          hugo
-          dart-sass
-        ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [pkgs.hugo pkgs.dart-sass];
+        };
       };
-    };
+
+      packages = rec {
+        website = pkgs.stdenv.mkDerivation {
+          name = "nakibrayan.com";
+          src = ./.;
+
+          buildInputs = [pkgs.hugo pkgs.dart-sass];
+
+          buildPhase = "hugo";
+
+          installPhase = "cp --recursive ./public $out";
+        };
+        default = website;
+      };
+    });
 }
